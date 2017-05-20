d728f705445a841d7d1a5344eb1d9f008022cf57
mirandae197 <mirandae197@potsdam.edu>
2017-05-19 23:20:06 -0400

    Problems
    Prime number generator isnt working!!

--- RSA.java
+++ RSA.java
@@ -25,10 +25,11 @@ public class RSA {
 	public RSA() {
 		createPQ();	//create prime numbers p and q
 		this.n = p*q;	// use p and q to make n
+		System.out.println(p + " " + q);
 		this.theta = (p-1)*(q-1);	// use p and q to make theta
 		findE();	//find an e where 1 < e < theta
-		this.d = e%theta;
 		pulverizer();	// used to find the x and y values
+		this.d = y%theta;
 	}
 	
 	// sets the p and q value to a prime number
@@ -42,17 +43,46 @@ public class RSA {
 	// makes sure that gcd(theta, e) == 1
 	public void findE() {
 		// for all numbers less than theta
-		for(int i = 3; i < this.theta; i--) {
+		System.out.println(theta);
+		for(int i = 3; i < this.theta; i++) {
+			System.out.println(i);
 			// if their gcd is 1 set e = i and break
 			if(gcd(this.theta, i) == 1) {
 				e = i;
 				break;
 			}
 		}
+		System.out.println(e);
 	}
 	
+	// finds our x and y values by using the
+	// pulverizer algorithm
 	public void pulverizer() {
-		
+		int [] pulv = new int[8];
+		pulv[0] = this.theta;	//temp theta value
+		pulv[1] = this.e;	// temp e value
+		pulv[2] = pulv[0]/pulv[1];	// quotient
+		pulv[3] = pulv[0]%pulv[1];	// remainder
+		pulv[4] = 1;	// x1
+		pulv[5] = 0;	// y1
+		pulv[6] = 0;	// x2
+		pulv[7] = 1;	// y2
+		while (pulv[3] != 0) {
+			pulv[0] = pulv[1];
+			pulv[1] = pulv[3];
+			int qTemp = pulv[2];
+			pulv[2] = pulv[0]/pulv[1];
+			pulv[3] = pulv[0]%pulv[1];
+			int x1Temp = pulv[4];
+			int y1Temp = pulv[5];
+			pulv[4] = pulv[6];	// x1
+			pulv[5] = pulv[7];	// y1
+			pulv[6] = x1Temp - qTemp*(pulv[4]);
+			pulv[7] = y1Temp - qTemp*(pulv[5]);
+		}
+		if(pulv[6]*this.theta + pulv[7]*this.e == 1)
+			System.out.println("Success");
+		else System.out.println("Fail");
 	}
 	
 	//finds as random a number i can think of
@@ -61,13 +91,9 @@ public class RSA {
 		Random r = new Random();	//create random object
 		int rand = r.nextInt(400)+1;	// create a random number
 		int prime = 1;	//keep track of our prime number
-		// temp is going to be the count for how many prime numbers
-		// will be multiplied together
-		int temp = ((r.nextInt(rand)+rand) + (r.nextInt(rand)+rand));
 		int primeCount = 3;
 		//from 0 to the temp number divided by a different random
-		//value every time, less than rand (because its /4)
-		for(int i = 0; i < temp/r.nextInt(rand/4); i++){
+		for(int i = 1; i < rand; i++){
 			//if the prime count value is prime then multiply it
 			if(isPrime(primeCount))
 				prime *= primeCount;
@@ -80,17 +106,17 @@ public class RSA {
 	// method to check if something is prime
 	//takes a prime value and returns a boolean
 	public boolean isPrime(int prime) {
-		if(n%2 == 0) return false;
+		if(prime%2 == 0) return false;
 		//looping through odd values until i^2
 		//is greater than the "prime" value
-		for(int i = 3; i*i <= prime; i +=2)
-			if(n%i == 0) return false; //if evenly divisible then return false
+		for(int i = 3; i <= prime; i++)
+			if(prime%i == 0) return false; //if evenly divisible then return false
 		return true; // else return true that it is prime
 	}
 	
 	// checking for the gcd recursively
 	public int gcd(int t, int i) {
-		if(t == 0 || i == 0) return a+b; //returning an assumed to be >0 value
+		if(t == 0 || i == 0) return t+i; //returning an assumed to be >0 value
 		return gcd(i, t%i); //returns gcd(smaller int, bigger int mod smaller)
 	}
 	
