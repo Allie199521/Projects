1fc08226d0b9a7aaced29b3dbc62dd9b04db3a9b
mirandae197 <mirandae197@potsdam.edu>
2017-05-19 21:41:24 -0400

    Editing
    beginning with finding prime numbers.

--- RSA.java
+++ RSA.java
@@ -7,7 +7,43 @@
 // RSA uses a public key, (n, e) to encrypt message m
 // to send to a private key, (n, d) to decrypt the encrypted message
 
-public class RSA {
+import java.util.*;
+import java.io.*;
 
+public class RSA {
+	public int n;
+	public int e;
+	private int d;
+	private int p;
+	private int q;
+	private int theta;
+	private int e;
+	private int x;
+	private int y;
 	
+	//implements the public and private keys
+	//by calling on a series of methods
+	public RSA() {
+		createPQ();
+	}
+	
+	 void createPQ() {
+		p = findPrimeNumber();
+	}
+	
+	public int findPrimeNumber() {
+		Random r = new Random();
+		int temp = r.nextInt(100) +10;
+		int firstPrime = 3;
+		for(int i = 0; i < temp; i++);
+			
+	}
+	
+}
+
+class RSAMain {
+	
+	public static void main(String [] args) {
+		RSA r = new RSA();
+	}
 }
