6235ee03edcfd5196a902bb8bcd681aedb28e5a4
mirandae197 <mirandae197@potsdam.edu>
2017-05-19 21:27:52 -0400

    Added RSA.java
    RSA.java contains RSA class and RSAMain class (for testing)

---
+++ RSA.java
@@ -0,0 +1,13 @@
+/* Name: Alexandra Miranda
+   File: RSA.java
+*/
+
+//this file encrypts and decrypts a message using the
+// public key encryption algorithm RSA
+// RSA uses a public key, (n, e) to encrypt message m
+// to send to a private key, (n, d) to decrypt the encrypted message
+
+public class RSA {
+
+	
+}
